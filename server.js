888caeabb31dd3c98401e9dbd5b352daf647836d
rotem-ziv21 <162511267+rotem-ziv21@users.noreply.github.com>
2025-07-06 16:56:48 +0300

    Fix path-to-regexp error in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
